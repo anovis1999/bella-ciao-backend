cadba900934c21af0407f2a0eb73c3e5cac4d75d
gil <anovis1999@gmail.com>
2021-01-28 16:12:13 +0200

    Added taeer feature

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 import search_takala
 import pull_takalot
 import takala_status
-
+import hashlib
+import time
+from fastapi.encoders import jsonable_encoder
+from mongodb_api import mongodb
+import takala
 
 app = FastAPI()
 
@@ -13,9 +17,19 @@ def is_alive():
     return {"status": "Alive"}
 
 
+@app.post("/random")
+def get_random_id():
+    hash = hashlib.sha1()
+    hash.update(str(time.time()).encode("utf-8"))
+    return hash.hexdigest()[:10]
+
 @app.post("/newtakala")
-def new_takala():
-    return "tair do your job please"
+def new_failure(params: takala.Item):
+    my_mongodb = mongodb("mongodb://localhost:27017/", "bella-ciao", "takala")
+    conn = my_mongodb.mongo_get_connection()
+    my_mongodb.mongo_insert_document(conn, jsonable_encoder(params))
+
+    return "tair do your job pls"
 
 
 @app.post("/searchtakala")
